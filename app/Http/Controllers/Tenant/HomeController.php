fa4022c6deefcc6192e9ae127bd083ecaba56532
komal-wes <komalburman.webethics@gmail.com>
2024-04-08 13:40:16 +0530

    Authorize logged in user across tenant subdomains

--- app/Http/Controllers/Tenant/HomeController.php
+++ app/Http/Controllers/Tenant/HomeController.php
@@ -3,17 +3,13 @@
 namespace App\Http\Controllers\Tenant;
 
 use App\Http\Controllers\Controller;
-use App\Models\Tenant;
-use App\Models\User;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index($email)
+    public function index()
     {
         return redirect(route('tenant.dashboard'));
     }
